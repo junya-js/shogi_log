f98ca0581c6e48b10f0c36b03f5cc726f09ca306
junya-js <ozakiyutaka1015@gmail.com>
2022-04-14 12:44:11 +0900

    second commit

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -10,9 +10,21 @@
 </head>
 
 <body>
-    <div class="flex justify-center">
+
+    <div class="flex justify-center relative">
+        <h2 class="absolute">将棋log</h2>
+        {{-- 画像の上にh2 --}}
         <img class="bg-cover min-h-screen" src=" {{ asset('image/istockphoto-617907652-612x612.jpeg') }}" alt="">
+        {{-- 画像の上にbutton --}}
+        <button
+            class="absolute gray-600 hover:bg-blue-400 text-black font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
+            Button
+        </button>
     </div>
+
+    <h3 class="text-red-600">What app is it?</h3>
+    <p>将棋ウォーズの棋譜を見ながら検討、気付きをまとめる将棋ノートアプリです</p>
+
 </body>
 
 </html>
