b61e9cb5abbc930345513fe0dcb4bfb7caae924b
junya-js <ozakiyutaka1015@gmail.com>
2022-04-25 06:57:33 +0900

    フォントとcssファイル読み込んだよ

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -6,40 +6,44 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="/css/app.css" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lora:ital,wght@1,600&display=swap" rel="stylesheet">
     <title>Document</title>
 </head>
 
 <body>
 
-        <div class="">
-            <section class="absolute w-full h-screen">
-                <img src=" {{ asset('image/EB16CA6D-B6D7-4A83-B9AB-42EAC7030639.jpg') }}" class="object-cover w-full h-full"
-                    alt="Image alt text" />
-            </section>
-            @if (Route::has('login'))
-            <div class="hidden fixed top-0 right-20 px-6 py-4 sm:block">
-                @auth
-                <a href="{{ url('/dashboard') }}" class="text-sm text-white underline">Dashboard</a>
-                @else
-                <a href="{{ route('login') }}" class="text-sm text-white underline">Log in</a>
-                @if (Route::has('register'))
-                <a href="{{ route('register') }}" class="ml-4 text-sm text-white underline">Register</a>
-                @endif
-                @endauth
-            </div>
+    <div class="">
+        <section class="absolute w-full h-screen">
+            <img src=" {{ asset('image/EB16CA6D-B6D7-4A83-B9AB-42EAC7030639.jpg') }}" class="object-cover w-full h-full"
+                alt="Image alt text" />
+        </section>
+        @if (Route::has('login'))
+        <div class="hidden fixed top-0 right-20 px-6 py-4 sm:block">
+            @auth
+            <a href="{{ url('/dashboard') }}" class="text-sm text-white underline">Dashboard</a>
+            @else
+            <a href="{{ route('login') }}" class="text-sm text-white underline">Log in</a>
+            @if (Route::has('register'))
+            <a href="{{ route('register') }}" class="ml-4 text-sm text-white underline">Register</a>
             @endif
-
-
-                <div class="absolute top-1/2 left-1/2 -translate-y-2/4 -translate-x-2/4 m-auto">
-                    <h2 class=" text-9xl text-white">Shogi_log</h2>
-                    <h3 class="mt-20 text-center text-xl text-orange-600">What app is it?</h3>
-                {{-- <div class="mt-10">
-                    <p class="text-center">将棋ウォーズの棋譜を見ながら検討、気付きをまとめる将棋ノートアプリです</p>
-                    <p class="text-center">ご自分の指した棋譜の重要ポイントをスクショし、その棋譜の内容と一緒に投稿したり、棋神のアドバイスをメモしたりするのにも使えます。</p>
-                </div> --}}
-            </div>
-
+            @endauth
         </div>
+        @endif
+
+
+        <div class="absolute top-1/2 left-1/2 -translate-y-2/4 -translate-x-2/4 m-auto">
+            <h2 class=" text-9xl text-white">Shogi_log</h2>
+            <h3 class="mt-20 text-center text-xl text-orange-600">What app is it?</h3>
+            {{-- <div class="mt-10">
+                <p class="text-center">将棋ウォーズの棋譜を見ながら検討、気付きをまとめる将棋ノートアプリです</p>
+                <p class="text-center">ご自分の指した棋譜の重要ポイントをスクショし、その棋譜の内容と一緒に投稿したり、棋神のアドバイスをメモしたりするのにも使えます。</p>
+            </div> --}}
+        </div>
+
+    </div>
 </body>
 
 </html>
