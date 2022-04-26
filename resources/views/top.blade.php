793b04127db75f1579e5f26b8ec2433da7f1316f
junya-js <ozakiyutaka1015@gmail.com>
2022-04-26 12:42:28 +0900

    アニメーション適用

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -7,6 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="/css/app.css" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="" href="{{ asset('js/style.js') }}">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Lora:ital,wght@1,600&display=swap" rel="stylesheet">
@@ -33,14 +34,20 @@
         </div>
         @endif
 
-
-        <div class="absolute top-1/2 left-1/2 -translate-y-2/4 -translate-x-2/4 m-auto">
-            <h2 class=" text-9xl text-white">Shogi_log</h2>
-            <h3 class="mt-20 text-center text-xl text-orange-600">What app is it?</h3>
-            {{-- <div class="mt-10">
-                <p class="text-center">将棋ウォーズの棋譜を見ながら検討、気付きをまとめる将棋ノートアプリです</p>
-                <p class="text-center">ご自分の指した棋譜の重要ポイントをスクショし、その棋譜の内容と一緒に投稿したり、棋神のアドバイスをメモしたりするのにも使えます。</p>
-            </div> --}}
+        <h2 class="absolute top-1/2 left-1/2 -translate-y-2/4 -translate-x-2/4 m-auto text-9xl text-white">Shogi_log
+        </h2>
+        <label
+            class="text-3xl underline text-orange-600 absolute top-3/4 left-1/2 -translate-y-2/4 -translate-x-2/4 m-auto open"
+            for="pop-up">
+            What app is it?
+        </label>
+        <input type="checkbox" id="pop-up">
+        <div class="overlay">
+            <div class="window">
+                <label class="close" for="pop-up">x</label>
+                <p class="text">将棋ウォーズの棋譜を見ながら検討、気付きをまとめる将棋ノートアプリです。<br>
+                    ご自分の指した棋譜の重要ポイントをスクショします。その棋譜の場面を検討し、メモを残したり、棋神のアドバイスをメモしたりするのにご利用ください。</p>
+            </div>
         </div>
 
     </div>
